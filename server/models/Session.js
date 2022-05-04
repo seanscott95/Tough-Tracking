0be0b4b2dfd87a984306d3f09c9baf7c69767f8b
Sean <seanms418@gmail.com>
2022-05-04 22:05:35 +1000

    added session model that includes a name, the strength and cardio activities and when its created

---
+++ server/models/Session.js
@@ -0,0 +1,27 @@
+const { Schema } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+
+const sessionSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  strength: {
+    type: Schema.Types.ObjectId,
+    ref: 'Strength',
+  },
+  cardio: {
+    type: Schema.Types.ObjectId,
+    ref: 'Cardio',
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: (timestamp) => dateFormat(timestamp),
+  },
+});
+
+const Session = mongoose.model('Session', sessionSchema);
+
+module.exports = Session;
