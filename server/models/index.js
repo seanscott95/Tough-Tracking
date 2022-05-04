83257d298a3b8d809c971aa6de1e23bcb9158659
Sean <seanms418@gmail.com>
2022-05-04 22:27:13 +1000

    spelling mistake for cardios require path

--- server/models/index.js
+++ server/models/index.js
@@ -1,6 +1,6 @@
 const User = require('./User');
 const Session = require('./Session');
 const Strength = require('./Strength');
-const Cardio = require('./UsCardioer');
+const Cardio = require('./Cardio');
 
 module.exports = { User, Session, Strength, Cardio };
