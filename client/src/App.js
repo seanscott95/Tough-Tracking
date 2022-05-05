446b7abfd6b5c65dc7788607218ed561ec4134a5
Sean <seanms418@gmail.com>
2022-05-05 23:40:31 +1000

    added routes using react router dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,31 @@
+import React, { useState } from 'react';
+import { Router, Route, Routes } from 'react-router-dom';
+
+import Navbar from './components/Navbar'
+import Home from './components/Home';
+import Login from './components/Login';
+import Dashboard from './components/Dashboard';
+import CreateWorkout from './components/CreateWorkout';
+import EditWorkout from './components/EditWorkout';
+import SingleWorkout from './components/SingleWorkout';
+
 function App() {
+  const [currentPage, setCurrentPage] = useState('Home');
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <Router>
+      <Routes>
+        <Navbar currentPage={currentPage} handlePageChange={handlePageChange} />
+          <Route exact path='/' component={Home}></Route>
+          <Route path='/login' component={Login}></Route>
+          <Route path='/dashboard' component={Dashboard}></Route>
+          <Route path='/createWorkout' component={CreateWorkout}></Route>
+          <Route path='/editWorkout' component={EditWorkout}></Route>
+          <Route path='/singleWorkout' component={SingleWorkout}></Route>
+      </Routes>
+    </Router>
   );
 }
 
