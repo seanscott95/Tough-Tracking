3d5af3425cf4dc79dc905518851af49df99cb575
Sean <seanms418@gmail.com>
2022-05-05 20:00:43 +1000

    added the mutations for the resolvers file, addUser, login, addSession, addExercise, removeSession, removeExercise

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -24,6 +24,53 @@ const resolvers = {
             return Session.findOne({ _id: sessionId });
         },
     },
+
+    Mutation: {
+        addUser: async (parent, { username, email, password }) => {
+            const user = await User.create({ username, email, password });
+            const token = signToken(user);
+            return { token, user };
+        },
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+            if (!user) {
+                throw new AuthenticationError('No user found with this email address');
+            }
+            
+            const correctPw = await user.isCorrectPassword(password);
+            if (!correctPw) {
+                throw new AuthenticationError('Incorrect credentials');
+            }
+            
+            const token = signToken(user);
+            return { token, user };
+        },
+        addSession: async (parent, { name }) => {
+            return Session.create({ name });
+        },
+        addExercise: async (parent, { sessionId, exercise, weight, sets, reps, distance, time, intensity }) => {
+            return Session.findOneAndUpdate(
+                { _id: sessionId },
+                {
+                    $addToSet: { exercise: { exercise, weight, sets, reps, distance, time, intensity } },
+                },
+                {
+                    new: true,
+                    runValidators: true,
+                }
+            );
+        },
+        removeSession: async (parent, { sessionId }) => {
+            return Session.findOneAndDelete({ _id: sessionId });
+        },
+        removeExercise: async (parent, { sessionId, exerciseId }) => {
+            return Session.findOneAndUpdate(
+                { _id: sessionId },
+                { $pull: { exercise: { _id: exerciseId } } },
+                { new: true }
+            );
+        },
+    },
 };
 
 module.exports = resolvers;
