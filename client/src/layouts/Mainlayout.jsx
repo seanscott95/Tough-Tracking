5850b69a1b3b6dc8d60f0f358129ee7131e0b108
Sean <seanms418@gmail.com>
2022-05-06 21:11:13 +1000

    covered props.children with the container component

--- client/src/layouts/Mainlayout.jsx
+++ client/src/layouts/Mainlayout.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import { Container } from '../components/styles/Container.styled'
 
 export default function Mainlayout(props) {
   return (
     <div>
-        
         {/* nav */}
         <Navbar></Navbar>
-
-        {props.children}
-
-
+        <Container>
+            {props.children}
+        </Container>
+        
         {/* footer */}
         
 
