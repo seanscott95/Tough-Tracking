d730f16243b7fcaf6b456e9481081fdca42799dc
Sean <seanms418@gmail.com>
2022-05-08 20:41:01 +1000

    created a strength card component that has a title of strength, displays weight, sets and reps and their values

---
+++ client/src/components/StrengthCard.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+export default function StrengthCard() {
+    return (
+        <>
+            <h2>Strength</h2>
+            <div>
+                <p>Exercise:</p>
+                <p>ExerciseName</p>
+            </div>
+            <div>
+                <p>Weight:</p>
+                <p>ExerciseWeight</p>
+            </div>
+            <div>
+                <p>Sets:</p>
+                <p>ExerciseSets</p>
+            </div>
+            <div>
+                <p>Reps:</p>
+                <p>ExerciseReps</p>
+            </div>
+        </>
+    )
+}
