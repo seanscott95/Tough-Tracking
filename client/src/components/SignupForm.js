0480921c7b91381b5c2a4df9977cf8f97981c5f7
Sean <seanms418@gmail.com>
2022-05-08 15:17:38 +1000

    add js functionality to the sign up form, will now handle form submition and add a user

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,27 +1,95 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+import { useMutation } from '@apollo/client';
+import { ADD_USER } from '../utils/mutations';
+import Auth from '../utils/auth';
 
 export default function SignupForm() {
+
+    const [formState, setFormState] = useState({
+        username: '',
+        email: '',
+        password: '',
+    });
+    const [addUser, { error, data }] = useMutation(ADD_USER);
+
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+
+        setFormState({
+            ...formState,
+            [name]: value,
+        });
+    };
+
+    const handleFormSubmit = async (event) => {
+        event.preventDefault();
+        console.log(formState);
+
+        try {
+            const { data } = await addUser({
+                variables: { ...formState },
+            });
+
+            Auth.login(data.addUser.token);
+        } catch (e) {
+            console.error(e);
+        }
+    };
+
     return (
         <>
-            <h2>Sign up</h2>
-            <form>
+            {data ? (
+                <p>
+                    Success! You may now head{' '}
+                    <Link to="/">back to the homepage.</Link>
+                </p>
+            ) : (
+                <>
+                    <h2>Sign up</h2>
+                    <form onSubmit={handleFormSubmit}>
+                        <div>
+                            <label for="username">Username:</label>
+                            <input
+                                type="text"
+                                name="username"
+                                placeholder="Enter your username"
+                                value={formState.username}
+                                onChange={handleChange}
+                                required />
+                        </div>
+                        <div >
+                            <label for="email">Email:</label>
+                            <input
+                                type="text"
+                                name="email"
+                                placeholder="Enter your email"
+                                value={formState.email}
+                                onChange={handleChange}
+                                required />
+                        </div>
+                        <div>
+                            <label for="password">Password:</label>
+                            <input
+                                type="password"
+                                name="password"
+                                placeholder="Password has to be longer than 8 characters"
+                                value={formState.password}
+                                onChange={handleChange}
+                                required />
+                        </div>
+                        <div>
+                            <button type="submit">Sign Up</button>
+                        </div>
+                    </form>
+                </>
+            )}
+            {error && (
                 <div>
-                    <label for="name-signup">Name:</label>
-                    <input type="text" id="name-signup" placeholder="Enter your name" required />
+                    {error.message}
                 </div>
-                <div >
-                    <label for="email-signup">Email:</label>
-                    <input type="text" id="email-signup" placeholder="Enter your email" required />
-                </div>
-                <div>
-                    <label for="password-signup">Password:</label>
-                    <input type="password" id="password-signup"
-                        placeholder="Password has to be longer than 8 characters" required />
-                </div>
-                <div>
-                    <button type="submit">Sign Up</button>
-                </div>
-            </form>
+            )}
         </>
     )
 }
