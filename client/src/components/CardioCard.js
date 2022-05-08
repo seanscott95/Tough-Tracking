ea83a42d12ebf4ccb9090ebc1792179963cbb782
Sean <seanms418@gmail.com>
2022-05-08 21:13:17 +1000

    created cardio card component displaying cardio as the title, exercise name, distance, time,  and intensity and their values, created boiler plate to benefit from visible layout

---
+++ client/src/components/CardioCard.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+export default function CardioCard() {
+    return (
+        <>
+            <h2>Cardio</h2>
+            <div>
+                <p>Exercise:</p>
+                <p>ExerciseName</p>
+            </div>
+            <div>
+                <p>Distance:</p>
+                <p>ExerciseDistance</p>
+            </div>
+            <div>
+                <p>Time:</p>
+                <p>ExerciseTime</p>
+            </div>
+            <div>
+                <p>Intensity:</p>
+                <p>ExerciseIntensity</p>
+            </div>
+        </>
+    )
+}
--- client/src/components/WorkoutCard.js
+++ client/src/components/WorkoutCard.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import CardioCard from './CardioCard'
+import StrengthCard from './StrengthCard'
 
 export default function WorkoutCard() {
   return (
     <>
+        {/* Display the workouts name and date
+            If workout has one strength and one cardio exercise,
+            display exercise and next to display exercise name for each*/}
         <div>
             <h2>WorkoutName - Date</h2>
             <div>
@@ -17,45 +22,13 @@ export default function WorkoutCard() {
             </div>
             <button>Edit</button>
         </div>
+
+        {/* Display exercise activity depending on the exercise type
+            If workout has 1 strength and 1 cardio workout, display one
+            strengthCard and one CardioCard  etc*/}
         <div>
-            <div>
-                <h2>ExerciseType</h2>
-                <div>
-                    <p>Exercise:</p>
-                    <p>ExerciseName</p>
-                </div>
-                <div>
-                    <p>Weight:</p>
-                    <p>ExerciseWeight</p>
-                </div>
-                <div>
-                    <p>Sets:</p>
-                    <p>ExerciseSets</p>
-                </div>
-                <div>
-                    <p>Reps:</p>
-                    <p>ExerciseReps</p>
-                </div>
-            </div>
-            <div>
-                <h2>ExerciseType</h2>
-                <div>
-                    <p>Exercise:</p>
-                    <p>ExerciseName</p>
-                </div>
-                <div>
-                    <p>Weight:</p>
-                    <p>ExerciseWeight</p>
-                </div>
-                <div>
-                    <p>Sets:</p>
-                    <p>ExerciseSets</p>
-                </div>
-                <div>
-                    <p>Reps:</p>
-                    <p>ExerciseReps</p>
-                </div>
-            </div>
+            <StrengthCard />
+            <CardioCard />
         </div>
     </>
   )
