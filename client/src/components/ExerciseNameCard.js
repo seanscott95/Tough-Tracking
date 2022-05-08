e1fd8be485b5f2815769a7ffc11e4446f2a89295
Sean <seanms418@gmail.com>
2022-05-08 21:46:28 +1000

    created exercise name card as it is going to be used on two pages, it will display the exercise and exercise name for each exercise in the workout, also has a button at the bottom to view workout

---
+++ client/src/components/ExerciseNameCard.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function ExerciseNameCard() {
+    return (
+        <>
+            <div>
+                {/* Display div containing exercise and exercise
+                name for every exercise */}
+                <div>
+                    <p>Exercise</p>
+                    <p>ExerciseName</p>
+                </div>
+            </div>
+            <button>View Workout</button>
+        </>
+    )
+}
--- client/src/components/pages/CreateWorkout.js
+++ client/src/components/pages/CreateWorkout.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import WorkoutForm from '../WorkoutForm';
+import ExerciseNameCard from '../ExerciseNameCard';
 
 export default function CreateWorkout() {
     return (
         <div>
+            <h2>Lets Begin!</h2>
+            <ExerciseNameCard />
             <WorkoutForm />
         </div>
     );
