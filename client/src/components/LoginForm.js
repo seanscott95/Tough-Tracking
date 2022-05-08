19759345e51b5f81d4a77063d0903d8289a49219
Sean <seanms418@gmail.com>
2022-05-08 15:25:11 +1000

    added js functionality to the login form

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,23 +1,80 @@
 import React from 'react'
 
 export default function LoginForm() {
+
+    const [formState, setFormState] = useState({ email: '', password: '' });
+    const [login, { error, data }] = useMutation(LOGIN_USER);
+
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+
+        setFormState({
+            ...formState,
+            [name]: value,
+        });
+    };
+
+    const handleFormSubmit = async (event) => {
+        event.preventDefault();
+        console.log(formState);
+        try {
+            const { data } = await login({
+                variables: { ...formState },
+            });
+
+            Auth.login(data.login.token);
+        } catch (e) {
+            console.error(e);
+        }
+
+        setFormState({
+            email: '',
+            password: '',
+        });
+    };
+
     return (
         <>
-            <h2>Login</h2>
-            <form>
+            {data ? (
+              <p>
+                Success! You may now head{' '}
+                <Link to="/">back to the homepage.</Link>
+              </p>
+            ) : (
+                <>
+                    <h2>Login</h2>
+                    <form onSubmit={handleFormSubmit}>
+                        <div>
+                            <label for="email">Email:</label>
+                            <input 
+                                type="text"
+                                name="email"
+                                placeholder="Enter your email"
+                                value={formState.email}
+                                onChange={handleChange}
+                                required />
+                        </div>
+                        <div>
+                            <label for="password">Password:</label>
+                            <input 
+                                type="password"
+                                name="password"
+                                placeholder="Your password should be longer than 5 characters"
+                                value={formState.password}
+                                onChange={handleChange}
+                                required />
+                        </div>
+                        <div>
+                            <button type="submit">Log In</button>
+                        </div>
+                    </form>
+                </>
+            )}
+            {error && (
                 <div>
-                    <label for="email-login">Email:</label>
-                    <input type="text" id="email-login" placeholder="Enter your email" required />
+                  {error.message}
                 </div>
-                <div>
-                    <label for="password-login">Password:</label>
-                    <input type="password" id="password-login"
-                        placeholder="Your password should be longer than 5 characters" required />
-                </div>
-                <div>
-                    <button type="submit">Log In</button>
-                </div>
-            </form>
+            )}
         </>
     )
-}
+};
