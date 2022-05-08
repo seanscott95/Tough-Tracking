6480df8f905118cee1d8ba2c1146ced770a13af3
Sean <seanms418@gmail.com>
2022-05-08 21:27:53 +1000

    added workout card component ot viewworkouts component page, deciding if view workouts is a neccessity, no js functionality did it to think about layout which is easier with visuals

--- client/src/components/pages/ViewWorkouts.js
+++ client/src/components/pages/ViewWorkouts.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import WorkoutCard from '../WorkoutCard';
+
 
 export default function ViewWorkouts() {
     return (
-        <div>
-            View Workouts
-        </div>
+        <>
+            <h2>Your Workouts</h2>
+            <p>Here's a detailed version of all your workouts in one place!</p>
+            {/* Display workout card component depending on the 
+                amount of workouts in db
+                Is it needed? Does display all information of workouts
+                not just a summarised version*/}
+            {/* May leave whole page for further production, display
+                workouts depending on the week, month, etc */}
+            <div>
+                {/* One WorkoutCard for every workout*/}
+                <WorkoutCard />
+            </div>
+        </>
     );
 }
