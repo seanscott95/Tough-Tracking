92b6f50cf6f234d993dee97124048406c06aa460
Sean <seanms418@gmail.com>
2022-05-09 16:09:53 +1000

    changed the signupform to go to the dashboard after signed in

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -37,55 +37,50 @@ export default function SignupForm() {
         } catch (e) {
             console.error(e);
         }
+
+        window.location.href = "/dashboard";
     };
 
     return (
         <>
-            {data ? (
-                <p>
-                    Success! You may now head{' '}
-                    <Link to="/">back to the homepage.</Link>
-                </p>
-            ) : (
-                <>
-                    <h2>Sign up</h2>
-                    <StyledForm onSubmit={handleFormSubmit}>
-                        <div>
-                            <label for="username">Username:</label>
-                            <input
-                                type="text"
-                                name="username"
-                                placeholder="Enter your username"
-                                value={formState.username}
-                                onChange={handleChange}
-                                required />
-                        </div>
-                        <div >
-                            <label for="email">Email:</label>
-                            <input
-                                type="text"
-                                name="email"
-                                placeholder="Enter your email"
-                                value={formState.email}
-                                onChange={handleChange}
-                                required />
-                        </div>
-                        <div>
-                            <label for="password">Password:</label>
-                            <input
-                                type="password"
-                                name="password"
-                                placeholder="Password has to be longer than 8 characters"
-                                value={formState.password}
-                                onChange={handleChange}
-                                required />
-                        </div>
-                        <div>
-                            <button type="submit">Sign Up</button>
-                        </div>
-                    </StyledForm>
-                </>
-            )}
+            <div>
+                <h2>Sign up</h2>
+                <StyledForm onSubmit={handleFormSubmit}>
+                    <div>
+                        <label for="username">Username:</label>
+                        <input
+                            type="text"
+                            name="username"
+                            placeholder="Enter your username"
+                            value={formState.username}
+                            onChange={handleChange}
+                            required />
+                    </div>
+                    <div >
+                        <label for="email">Email:</label>
+                        <input
+                            type="text"
+                            name="email"
+                            placeholder="Enter your email"
+                            value={formState.email}
+                            onChange={handleChange}
+                            required />
+                    </div>
+                    <div>
+                        <label for="password">Password:</label>
+                        <input
+                            type="password"
+                            name="password"
+                            placeholder="Password has to be longer than 8 characters"
+                            value={formState.password}
+                            onChange={handleChange}
+                            required />
+                    </div>
+                    <div>
+                        <button type="submit">Sign Up</button>
+                    </div>
+                </StyledForm>
+            </div>
             {error && (
                 <div>
                     {error.message}
