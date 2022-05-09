2408fc2260c3d0b90f09157633ef1b4047eb8865
Sean <seanms418@gmail.com>
2022-05-09 14:53:15 +1000

    chanigng up layout deleted a component that was used in create workout

--- client/src/components/pages/CreateWorkout.js
+++ client/src/components/pages/CreateWorkout.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import WorkoutForm from '../WorkoutForm';
-import ExerciseNameCard from '../ExerciseNameCard';
 
 export default function CreateWorkout() {
     return (
         <div>
             <h2>Lets Begin!</h2>
-            <ExerciseNameCard />
+            <div>
+                {/* Display div containing exercise and exercise name for 
+                every exercise once added by the workout form component below */}
+                <div>
+                    <p>Exercise</p>
+                    <p>ExerciseName</p>
+                </div>
+            </div>
+            <button>View Workout</button>
             <WorkoutForm />
         </div>
     );
