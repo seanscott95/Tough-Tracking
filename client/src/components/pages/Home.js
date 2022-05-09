7c0c2d342822cdf90e258c887ac45d72bb0ccb7b
Sean <seanms418@gmail.com>
2022-05-09 14:22:46 +1000

    added html to the home page, not stylised, p tag needs changing to an img of the finalised app

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -2,8 +2,29 @@ import React from 'react';
 
 export default function Home() {
     return (
-        <div>
-            Home
-        </div>
+        <>
+            <div>
+                <q>Less Thinking.<span>More Training.</span></q>
+                <div>
+                    <div>
+                        &rarr;
+                        <p>Visualise and plan your workout ahead.</p>
+                    </div>
+                    <div>
+                        &rarr;
+                        <p>Log Strength and cardio workouts..</p>
+                    </div>
+                    <div>
+                        &rarr;
+                        <p>Edit and delete past workouts.</p>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <p>Login or signup for free to access full benefits.</p>
+                {/* P tag below will be a photo of finished product */}
+                <p>Screenshot of app</p>
+            </div>
+        </>
     );
 }
