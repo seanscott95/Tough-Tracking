d7b9339d66ffaeb98b7ad0b8c4e59b49af193aa6
Sean <seanms418@gmail.com>
2022-05-12 17:10:40 +1000

    imported useNaviaget from react router dom, created view button that for each workout that has the value of the workout id, button handler function takes the values of the button being the id and uses the navigate hook to go to the viewSingle page with the url params as the value of the button being the workout id

--- client/src/components/WorkoutCardSmall.js
+++ client/src/components/WorkoutCardSmall.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useQuery } from '@apollo/client';
+import { useNavigate  } from "react-router-dom";
 
 import { QUERY_WORKOUTS } from '../utils/queries'
 
@@ -7,8 +8,14 @@ export default function WorkoutCardSmall() {
 
   const { loading, data } = useQuery(QUERY_WORKOUTS);
   const temp = data?.getWorkouts
-  console.log('temp', temp)
-  console.log('data', data)
+
+  let navigate  = useNavigate();
+
+  function handleClick(e) {
+    e.preventDefault();
+    const id = e.target.value;
+    navigate(`/viewSingle/${id}`);
+  }
 
   if (loading) {
     return <div>Sorry, still loading</div>
@@ -33,7 +40,12 @@ export default function WorkoutCardSmall() {
                 })}
               </ul>
             </li>
-            <button>View</button>
+            <button
+              key={item._id}
+              id={item._id}
+              value={item._id}
+              onClick={handleClick}
+            >View</button>
           </ul>
         ))}
       </div>
