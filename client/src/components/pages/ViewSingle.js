aa8ef20c3466b3bf3adf06f2a00300a9e049c909
Sean <seanms418@gmail.com>
2022-05-12 14:58:26 +1000

    added useParams and useQuery hooks to grab the workout id that will be in the url, that id is then used in the query hook querying the single workout, sending it down to the component workout, for not console log will display if its working or not once the buttons to lead to this page are implemented

--- client/src/components/pages/ViewSingle.js
+++ client/src/components/pages/ViewSingle.js
@@ -1,15 +1,33 @@
 import React from 'react'
 
-import WorkoutSingle from '../WorkoutSingle'
+import { useParams } from 'react-router-dom';
+import { useQuery } from '@apollo/client';
+import WorkoutSingle from '../WorkoutSingle';
+import { QUERY_SINGLE_WORKOUT } from '../../utils/queries';
+
 
 export default function ViewSingle() {
+  const { workoutId } = useParams();
+
+  const { loading, data } = useQuery(QUERY_SINGLE_WORKOUT, {
+    variables: { workoutId: workoutId },
+  });
+
+  console.log(data)
+  const workout = data?.workout || {};
+  console.log('workout', workout);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <>
 
-        {/* Views a single workout */}
-        <div>
-            <WorkoutSingle />
-        </div>
+      {/* Views a single workout */}
+      <div>
+        <WorkoutSingle workout={workout}/>
+      </div>
     </>
   )
 }
