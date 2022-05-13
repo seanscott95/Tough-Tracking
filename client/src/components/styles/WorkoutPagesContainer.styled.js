a73c513bc57f1bcb2940d26a878ea582792a6555
Sean <seanms418@gmail.com>
2022-05-13 23:58:58 +1000

    removed button class

--- client/src/components/styles/WorkoutPagesContainer.styled.js
+++ client/src/components/styles/WorkoutPagesContainer.styled.js
@@ -1,7 +1,6 @@
 import styled from "styled-components";
 
 export const WorkoutPagesContainer = styled.div`
-    
     padding: 20px;
     font-size: 16px;
     display: flex;
@@ -10,14 +9,4 @@ export const WorkoutPagesContainer = styled.div`
     justify-content: center;
     align-items: center;
     text-align: center;
-    
-    button {
-        padding: 1px 5px;
-        margin: 5px 0px;
-        background: #1e2327;
-        color: white;
-        font-weight: bold;
-        border-radius: 10px;
-        cursor: pointer;
-    }
 `
