9e90d7b1c017e0d6a065faec44ca9737caaafd7e
Sean <seanms418@gmail.com>
2022-05-13 20:38:04 +1000

    created a main styled component to fit the pages, added in align and justify to the center so the content is center of screen, added that component to mainlayout

---
+++ client/src/components/styles/Main.styled.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const MainStyle = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    min-height: 100vh;
+`
--- client/src/layouts/Mainlayout.jsx
+++ client/src/layouts/Mainlayout.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import { Container } from '../components/styles/Container.styled'
-
+import { MainStyle } from '../components/styles/Main.styled';
 export default function Mainlayout(props) {
   return (
-    <>
+    <MainStyle>
         <Navbar />
         <Container>
             {props.children}
         </Container>
         <Footer />
-    </>
+    </MainStyle>
   )
 }
