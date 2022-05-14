49ef038546829abf5eb9cb64b0c9fc8cc2d3e8c2
Sean <seanms418@gmail.com>
2022-05-14 18:28:42 +1000

    added a background image and put it in the main styled component, container styled component added max-height 100%

--- client/src/components/styles/Main.styled.js
+++ client/src/components/styles/Main.styled.js
@@ -1,10 +1,17 @@
 import styled from 'styled-components';
 
+import backgroundImg from '../../assets/images/barbellLift.png';
+
 export const MainStyle = styled.div`
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;
-    margin: 80px 0px;
-    min-height: calc(100% - 138px)
+    margin-top: 59px;
+    margin-bottom: 79px;
+    background-image: url(${backgroundImg});
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: cover;
+    min-height: calc(100vh - 138px);
 `
