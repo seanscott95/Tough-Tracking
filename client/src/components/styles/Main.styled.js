87fd31dcdf21ce967d3a49bdc2fd2c7e46f967f8
Sean <seanms418@gmail.com>
2022-05-14 15:40:37 +1000

    added a min-height to the main styled component 100% minus the height of the footer and nav

--- client/src/components/styles/Main.styled.js
+++ client/src/components/styles/Main.styled.js
@@ -4,5 +4,7 @@ export const MainStyle = styled.div`
     display: flex;
     flex-direction: column;
     justify-content: center;
+    align-items: center;
     margin: 80px 0px;
+    min-height: calc(100% - 138px)
 `
