64ebb101597fb861286f98667133ad30d866e559
Sean <seanms418@gmail.com>
2022-05-14 18:36:41 +1000

    added borders to the card

--- client/src/components/styles/Card.styled.js
+++ client/src/components/styles/Card.styled.js
@@ -2,14 +2,16 @@ import styled from "styled-components";
 
 export const StyledCard = styled.div`
     margin: 5px;
-    background: rgb(30, 35, 39, .9);
+    background: rgb(255, 255, 255, 0.6);
     border-radius: 10px;
+    border: 1px solid black;
 
     .card {
-        background: rgb(255, 255, 255, 0.6);
+        background: rgb(30, 35, 39, .9);
         border-radius: 5px;
         padding: 20px;
         margin: 5px;
         padding: 10px;
+        border: 1px solid black;
     }
 `
