eb97543acc5aceccfbcdd511ec8bdd6a76efebcb
Sean <seanms418@gmail.com>
2022-06-08 16:19:16 +1000

    created a flex component that has multiple styled common flex box properties using styled components

---
+++ client/src/components/styles/Flex.styled.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const Flex = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+`
+
+export const FlexColumn = styled(Flex)`
+    flex-direction: column;
+`
+
+export const FlexTop = styled(Flex)`
+    justify-content: top;
+    align-items: start;
+`
