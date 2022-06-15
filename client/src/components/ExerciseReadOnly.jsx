0fadd7514e9c5613277ad4c55805c533d00f9f61
Sean <seanms418@gmail.com>
2022-06-15 16:57:29 +1000

    changed the unordered list into an ordered list to change the styling used in global for ordered lists

--- client/src/components/ExerciseReadOnly.jsx
+++ client/src/components/ExerciseReadOnly.jsx
@@ -8,18 +8,18 @@ export default function ExerciseReadOnly({ exercises }) {
                 <li>{exercises.name}</li>
                 <li>{exercises.type}</li>
                 {exercises.type === 'Strength' ?
-                    <>
+                    <ol>
                         <li>{`${exercises.weight} kg's`}</li>
                         <li>{`${exercises.sets} sets`}</li>
                         <li>{`${exercises.reps} reps`}</li>
-                    </>
+                    </ol>
                     : ''}
                 {exercises.type === 'Cardio' ?
-                    <>
+                    <ol>
                         <li>{`${exercises.distance} km's`}</li>
                         <li>{`${exercises.time} min`}</li>
                         <li>{exercises.intensity}</li>
-                    </>
+                    </ol>
                     : ''}
             </ul>
         </CardInner>
