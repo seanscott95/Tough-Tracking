339e8ef0de1aa384d3c593442fc0a80f49627edb
Sean <seanms418@gmail.com>
2022-06-17 14:49:45 +1000

    addedd properties to the slider styled component of the swiper component, added properties that center exercise cards, shows a little of the next one to indicate there are more to swipe, added space between them, there is  a difference in some of the workouts exercise cards margin, needs polishing

--- client/src/components/SwiperContainer.jsx
+++ client/src/components/SwiperContainer.jsx
@@ -2,20 +2,20 @@ import React from 'react'
 
 import ExerciseReadOnly from './ExerciseReadOnly';
 
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { SwiperSlide } from 'swiper/react';
 import 'swiper/swiper-bundle.min.css';
 import 'swiper/swiper.min.css';
 
-import { Slider, SliderItem } from './styles/Slider.styled';
-import { StyledCard, CardInner } from './styles/Card.styled';
+import { Slider } from './styles/Slider.styled';
+import { StyledCard } from './styles/Card.styled';
 
 export default function SwiperContainer({ item }) {
     return (
 
         <Slider
-            slidesPerView={'auto'}
+            slidesPerView={1.6}
             centeredSlides
-            spaceBetween={2}
+            spaceBetween={30}
         >
             {item.exercises.map((exercises) => (
                 <SwiperSlide>
