1afb91f8bbebd4ba32747db12c663d4a10746cd9
Sean <seanms418@gmail.com>
2022-06-28 16:24:03 +1000

    created a media query for the slidder making the width smaller

--- client/src/components/styles/Slider.styled.js
+++ client/src/components/styles/Slider.styled.js
@@ -4,4 +4,8 @@ import { Swiper } from 'swiper/react';
 export const Slider = styled(Swiper)`
     height: fit-content;
     width: 300px;
+
+    @media screen and (max-width: 367px) {
+        width: 200px;
+    }
 `
