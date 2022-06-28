547ac88b76da1ef05b15e134fac42c86036b7b74
Sean <seanms418@gmail.com>
2022-06-28 17:07:42 +1000

    deleted the paragraph explaining how to edit below the title

--- client/src/components/WorkoutSingle.jsx
+++ client/src/components/WorkoutSingle.jsx
@@ -11,29 +11,23 @@ export default function WorkoutSingle({
   handleNameChange }) {
 
   return (
-    <div>
-      <FlexColumn>
-        <h3>Edit your workout:</h3>
-        <div>
-          <p>Once you have edited all your exercises click save workout to finish.</p>
-          <div>
-            <p>Workout Name:</p>
-            <input
-              type="text"
-              name="workoutName"
-              placeholder='Sunday, Gym, Workout...'
-              onChange={handleNameChange}
-              value={workoutName}
-              required />
-          </div>
-        </div>
-        
-        <ExerciseSingle
-          exercises={exercises}
-          workoutForm={workoutForm}
-          handleExerciseChange={handleExerciseChange}>
-        </ExerciseSingle>
-      </FlexColumn>
-    </div>
+    <FlexColumn>
+      <h3>Edit your workout:</h3>
+      <div>
+        <p>Workout Name:</p>
+        <input
+          type="text"
+          name="workoutName"
+          placeholder='Sunday, Gym, Workout...'
+          onChange={handleNameChange}
+          value={workoutName}
+          required />
+      </div>
+      <ExerciseSingle
+        exercises={exercises}
+        workoutForm={workoutForm}
+        handleExerciseChange={handleExerciseChange}>
+      </ExerciseSingle>
+    </FlexColumn>
   )
 }
